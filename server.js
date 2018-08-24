4accc49f658c7f15d6dbb11775b1dc5efac590ee
Efren <efren.alvarezlamolda@takeaway.com>
2018-08-24 11:41:59 +0200

    Enable Cross origin

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ var eventController = require('./controllers/eventController.js');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+//Allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Auth");
+  next();
+});
+
 // #### Event Domain ####
 
 app.post('/event', eventController.createEvent);
