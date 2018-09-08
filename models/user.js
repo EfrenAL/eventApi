fe2e8e128dd88a2f44914bb80a977d9188378b78
Efren Alvarez Lamolda <efren@efrens-mbp.localdomain>
2018-09-08 10:00:42 +0200

    Increase length of pictureUrl2

--- models/user.js
+++ models/user.js
@@ -56,7 +56,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: true,
             validate: {
-                len: [1, 100]
+                len: [1, 300]
             }
         }
     }, {
