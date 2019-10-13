3e7058e8f628ee37b5bcf17710d4b6ac2846e88f
EfrenAl <eal@tier.app>
2019-10-13 10:23:10 +0200

    Test

--- models/coworking.js
+++ models/coworking.js
@@ -41,14 +41,14 @@ module.exports = function(sequelize, DataTypes){
             type: DataTypes.STRING,
             allowNull: true,
             validate: {
-                len: [1, 100]
+                len: [1, 200]
             }
         },
         webUrl: {
             type: DataTypes.STRING,
             allowNull: true,
             validate: {
-                len: [1, 100]
+                len: [1, 200]
             }
         }
     });
